a91f8a6bdfa0b4c44200716dbbb4376f4939c41c
anshunli <divfooter@163.com>
2023-07-14 17:22:36 +0800

    asdf

--- api/hello.ts
+++ api/hello.ts
@@ -13,18 +13,23 @@ const pinecone = new PineconeClient();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   await pinecone.init(myEnv.pinecone);
+  if(!req.query.code){
+    res.send({ code: -1,data:[],error:"code is required"});
+    return;
+  }
+  
   try {
-    const index = pinecone.Index("pdf"); //change to your own index name
-    const embeddings = new OpenAIEmbeddings(myEnv.openai);
-    await PineconeStore.fromDocuments(docs, embeddings, {
-      pineconeIndex: index,
-      namespace: fileName,
-      textKey: 'text',
+    const index = pinecone.Index("pdf");
+    const indexStats = await index.describeIndexStats({
+      describeIndexStatsRequest: {
+        filter: {},
+      },
     });
-    console.log('writeToFile complete:' + fileName);
-    return true;
+    // console.log(Object.keys(indexStats.namespaces));
+    res.send({ code: 200,data:Object.keys(indexStats.namespaces).filter(item=>{
+      return item.startsWith(req.query.code)
+    })});
   } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to ingest your data');
+    res.send({ code: -1,data:[],error});
   }
 }
