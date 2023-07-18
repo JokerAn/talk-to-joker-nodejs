010d8f183bf73b7a5fef2cfb98e5e7887737a61e
安顺利 <anshunli1@jd.com>
2023-07-18 19:52:09 +0800

    新增环境变量

--- api/indexNameSpace.ts
+++ api/indexNameSpace.ts
@@ -1,10 +1,14 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { PineconeClient } from '@pinecone-database/pinecone';
-import cors from 'cors';
-let myEnv:any= {}
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import cors from "cors";
+import { env } from "process";
+let myEnv: any = {};
 const pinecone = new PineconeClient();
+// console.log(env.PINECONE_ENVIRONMENT);
+// console.log(env.PINECONE_API_KEY);
+// console.log(env);
 function getUrlParamsFor(url = "", name?: string) {
   const queryParams: any = url.split("?")[1] || [];
   let finallyData: any = {};
@@ -22,41 +26,42 @@ export default async function handler(req: any, res: any) {
   await new Promise((resolve, reject) => {
     cors()(req, res, (err) => {
       if (err) {
-				console.log('cors error',err)
+        console.log("cors error", err);
         reject(err);
       } else {
         resolve(req);
       }
     });
   });
-  let paramss:any=getUrlParamsFor(req.url);
-  console.log(paramss)
-	let code: string = getUrlParamsFor(req.url, "code");
-	let pineconeApiKey: string = getUrlParamsFor(req.url, "pineconeApiKey");
-	let pineconeEnvironment: string = getUrlParamsFor(req.url, "environment");
-  if (!code||!pineconeApiKey||!pineconeEnvironment) {
-    res.send({ code: -1, data: [], error: "code\pineconeApiKey\pineconeEnvironment is required" });
+  let paramss: any = getUrlParamsFor(req.url);
+  console.log(paramss);
+  let code: string = getUrlParamsFor(req.url, "code");
+  if (!code) {
+    res.send({ code: -1, data: [], error: "code is required" });
     return;
   }
   myEnv = {
     pinecone: {
-      environment: pineconeEnvironment,
-      apiKey: pineconeApiKey
-    }
-  }
+      environment: env.PINECONE_ENVIRONMENT,
+      apiKey: env.PINECONE_API_KEY,
+    },
+  };
   await pinecone.init(myEnv.pinecone);
   try {
     const index = pinecone.Index("pdf");
-    const indexStats:any = await index.describeIndexStats({
+    const indexStats: any = await index.describeIndexStats({
       describeIndexStatsRequest: {
         filter: {},
       },
     });
     // console.log(Object.keys(indexStats.namespaces));
-    res.send({ code: 200,data:Object.keys(indexStats.namespaces).filter(item=>{
-      return item.startsWith(req.query.code)
-    })});
+    res.send({
+      code: 200,
+      data: Object.keys(indexStats.namespaces).filter((item) => {
+        return item.startsWith(req.query.code);
+      }),
+    });
   } catch (error) {
-    res.send({ code: -1,data:[],error});
+    res.send({ code: -1, data: [], error });
   }
 }
--- api/nextPdf.ts
+++ api/nextPdf.ts
@@ -1,33 +1,34 @@
-import formidable from 'formidable-serverless';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import pdf from 'pdf-parse/lib/pdf-parse.js'
-import cors from 'cors';
+import formidable from "formidable-serverless";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import pdf from "pdf-parse/lib/pdf-parse.js";
+import cors from "cors";
 
 import { Document } from "langchain/document";
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { PineconeClient } from '@pinecone-database/pinecone';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { env } from "process";
 
 export const config = {
-	api: {
-	  externalResolver: true,
-	},
+  api: {
+    externalResolver: true,
+  },
+};
+let myEnv: any = {};
+const processDocuments = async (docs, fileName, index) => {
+  // console.log(fileName)
+  try {
+    const embeddings = new OpenAIEmbeddings(myEnv.openai);
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: fileName,
+      textKey: "text",
+    });
+    console.log("writeToFile complete:" + fileName);
+    return true;
+  } catch (error) {
+    console.log("error", error);
   }
-let myEnv:any={};
-const processDocuments = async (docs, fileName,index) => {
-	// console.log(fileName)
-	try {
-		const embeddings = new OpenAIEmbeddings(myEnv.openai);
-		await PineconeStore.fromDocuments(docs, embeddings, {
-			pineconeIndex: index,
-			namespace: fileName,
-			textKey: 'text',
-		});
-		console.log('writeToFile complete:' + fileName);
-		return true;
-	} catch (error) {
-		console.log('error', error);
-	}
 };
 function getUrlParamsFor(url = "", name?: string) {
   const queryParams: any = url.split("?")[1] || [];
@@ -42,41 +43,41 @@ function getUrlParamsFor(url = "", name?: string) {
   }
   return finallyData;
 }
-export default async(req, res,next) => {
-	await new Promise((resolve, reject) => {
+export default async (req, res, next) => {
+  await new Promise((resolve, reject) => {
     cors()(req, res, (err) => {
       if (err) {
-				console.log('cors error',err)
+        console.log("cors error", err);
         reject(err);
       } else {
         resolve(req);
       }
     });
   });
-	let paramss:any=getUrlParamsFor(req.url);
-  console.log(paramss)
-	let fileName: string = getUrlParamsFor(req.url, "fileName");
-	let openAIApiKey: string = getUrlParamsFor(req.url, "openAIApiKey");
-	let pineconeApiKey: string = getUrlParamsFor(req.url, "pineconeApiKey");
-	let pineconeEnvironment: string = getUrlParamsFor(req.url, "environment");
-  if (!fileName||!openAIApiKey||!pineconeApiKey||!pineconeEnvironment) {
-    res.send({ code: -1, data: [], error: "fileName\openAIApiKey\pineconeApiKey\pineconeEnvironment is required" });
+  let paramss: any = getUrlParamsFor(req.url);
+  console.log(paramss);
+  let fileName: string = getUrlParamsFor(req.url, "fileName");
+  // let openAIApiKey: string = getUrlParamsFor(req.url, "openAIApiKey");
+  // let pineconeApiKey: string = getUrlParamsFor(req.url, "pineconeApiKey");
+  // let pineconeEnvironment: string = getUrlParamsFor(req.url, "environment");
+  if (!fileName) {
+    res.send({ code: -1, data: [], error: "fileName is required" });
     return;
   }
   myEnv = {
     pinecone: {
-      environment: pineconeEnvironment,
-      apiKey: pineconeApiKey
+      environment: env.PINECONE_ENVIRONMENT,
+      apiKey: env.PINECONE_API_KEY,
     },
     openai: {
-      openAIApiKey: openAIApiKey
-    }
-  }
-	console.log('myEnv',myEnv)
-	console.log('req.query')
-	const pinecone = new PineconeClient();
-	await pinecone.init(myEnv.pinecone);
-	const index = await pinecone.Index("pdf");
+      openAIApiKey: env.OPENAI_API_KEY,
+    },
+  };
+  // console.log("myEnv", myEnv);
+  // console.log("req.query");
+  const pinecone = new PineconeClient();
+  await pinecone.init(myEnv.pinecone);
+  const index = await pinecone.Index("pdf");
   const form = new formidable.IncomingForm();
   form.keepExtensions = true;
 
@@ -84,19 +85,17 @@ export default async(req, res,next) => {
     if (err) {
       return res.status(400).json({ message: err });
     }
-	console.log(files);
-	const pdfData = await pdf(files.file.path);
-	console.log(pdfData)
-	const textSplitter = new RecursiveCharacterTextSplitter({
-		chunkSize: 1000,
-		chunkOverlap: 200,
-	});
+    console.log(files);
+    const pdfData = await pdf(files.file.path);
+    console.log(pdfData);
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
 
-	const docs = await textSplitter.splitDocuments([
-		new Document({ pageContent: pdfData.text }),
-	]);
-	console.log(docs);
-	await processDocuments(docs, fileName,index);
-	res.send({ code: 200,files });
+    const docs = await textSplitter.splitDocuments([new Document({ pageContent: pdfData.text })]);
+    console.log(docs);
+    await processDocuments(docs, fileName, index);
+    res.send({ code: 200, files });
   });
-}
+};
