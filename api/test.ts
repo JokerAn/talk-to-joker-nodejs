b2e0b1b4958c3902e0eb0882f3a393a1387b9878
安顺利 <anshunli1@jd.com>
2023-07-18 20:18:09 +0800

    去除无用代码

--- api/test.ts
+++ api/test.ts
@@ -1,52 +1,52 @@
-import formidable from 'formidable-serverless';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import pdf from 'pdf-parse/lib/pdf-parse.js'
+import formidable from "formidable-serverless";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import pdf from "pdf-parse/lib/pdf-parse.js";
 
 import { Document } from "langchain/document";
-import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { PineconeClient } from '@pinecone-database/pinecone';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 const myEnv = {
-	pinecone: {
-		environment: 'us-west4-gcp-free',
-		apiKey: '6df035c5-7ede-4f2e-8a03-07e215c033c6'
-	},
-	openai: {
-		openAIApiKey: "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"
-	}
-}
+  pinecone: {
+    environment: "us-west4-gcp-free",
+    apiKey: "6df035c5-7ede-4f2e-8a03-07e215c033c6",
+  },
+  openai: {
+    openAIApiKey: "",
+  },
+};
 
 export const config = {
-	api: {
-	  externalResolver: true,
-	},
-  }
-
-const processDocuments = async (docs, fileName,index) => {
-	// console.log(fileName)
-	try {
-		const embeddings = new OpenAIEmbeddings(myEnv.openai);
-		await PineconeStore.fromDocuments(docs, embeddings, {
-			pineconeIndex: index,
-			namespace: fileName,
-			textKey: 'text',
-		});
-		console.log('writeToFile complete:' + fileName);
-		return true;
-	} catch (error) {
-		console.log('error', error);
-	}
+  api: {
+    externalResolver: true,
+  },
 };
-export default async(req, res) => {
-	if(!req.query.pathName){
-		res.send({ code: -1,data:[],error:"pathName is required"});
-		return;
-	}
-	console.log(req.query)
-	return res.send({ code: 200,files:[] });
-	const pinecone = new PineconeClient();
-	await pinecone.init(myEnv.pinecone);
-	const index = await pinecone.Index("pdf");
+
+const processDocuments = async (docs, fileName, index) => {
+  // console.log(fileName)
+  try {
+    const embeddings = new OpenAIEmbeddings(myEnv.openai);
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: fileName,
+      textKey: "text",
+    });
+    console.log("writeToFile complete:" + fileName);
+    return true;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+export default async (req, res) => {
+  if (!req.query.pathName) {
+    res.send({ code: -1, data: [], error: "pathName is required" });
+    return;
+  }
+  console.log(req.query);
+  return res.send({ code: 200, files: [] });
+  const pinecone = new PineconeClient();
+  await pinecone.init(myEnv.pinecone);
+  const index = await pinecone.Index("pdf");
   const form = new formidable.IncomingForm();
   form.keepExtensions = true;
 
@@ -54,19 +54,17 @@ export default async(req, res) => {
     if (err) {
       return res.status(400).json({ message: err });
     }
-	console.log(files);
-	const pdfData = await pdf(files.file.path);
-	console.log(pdfData)
-	const textSplitter = new RecursiveCharacterTextSplitter({
-		chunkSize: 1000,
-		chunkOverlap: 200,
-	});
+    console.log(files);
+    const pdfData = await pdf(files.file.path);
+    console.log(pdfData);
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
 
-	const docs = await textSplitter.splitDocuments([
-		new Document({ pageContent: pdfData.text }),
-	]);
-	console.log(docs);
-	await processDocuments(docs, 'anshunli-1212',index);
-	res.send({ code: 200,files });
+    const docs = await textSplitter.splitDocuments([new Document({ pageContent: pdfData.text })]);
+    console.log(docs);
+    await processDocuments(docs, "anshunli-1212", index);
+    res.send({ code: 200, files });
   });
-}
+};
